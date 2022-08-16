c47159c4d2783a3655c8b352ab2f108a6e9b8c1d
Timothy <timothy.2k21@gmail.com>
2022-08-16 13:56:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+function Scuber() {
+    const companyName ='scuber';
+}
+let mostProfitableNeighborhood = 'Chelsea';
+function Chelsea() {
+    let mostProfitableNeighborhood = Chelsea;
+}
+let companyCeo = 'Susan Smith';
+
